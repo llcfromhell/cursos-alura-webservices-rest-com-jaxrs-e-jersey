e6832de5f7d21b38367d9e348926d4d665e2eb9e
Lourenço Lima da Cunha <llc.from.hell@gmail.com>
2016-02-11 00:33:22 -0200

    adição de código do exercício 11

--- loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,6 +16,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -43,5 +45,15 @@ public class CarrinhoResource {
 		return Response.created(uri).build();
 		
 	}
+	
+	@Path("{id}/produtos/{produtoId}/quantidade")
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response alteraProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId, String conteudo) {
+        Carrinho carrinho = new CarrinhoDAO().busca(id);
+        Produto produto = (Produto) new XStream().fromXML(conteudo);
+        carrinho.trocaQuantidade(produto);
+        return Response.ok().build();
+    }
 	
 }
